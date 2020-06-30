9a2ca1418eaa87673799df3a712055658afeaf83
RoXoM <xyy94813@sina.com>
2020-06-30 19:00:58 +0800

    fix: crash by use commander args

--- bin/china-address-code.js
+++ bin/china-address-code.js
@@ -1,30 +1,34 @@
 #!/usr/bin/env node
-const program = require("commander");
-const path = require("path");
-const fs = require("fs");
-const main = require("../lib/core");
-const { version } = require("../package.json");
+const program = require('commander');
+const path = require('path');
+const fs = require('fs');
+const main = require('../lib/core');
+const { version } = require('../package.json');
 
-const resolveArgPath = val => path.resolve(process.cwd(), val);
+const resolveArgPath = (val) => path.resolve(process.cwd(), val);
 
 program
-  .version(version, "-v, --version")
-  .usage("[options] <file ...>")
-  .option(
-    "-c, --chromium <file>",
-    "The chromium path",
-    resolveArgPath,
-    process.env.CHROMIUM_PATH
-  )
-  .option(
-    "-o, --output <dir>",
-    "The output dir path",
-    resolveArgPath,
-    process.env.OUTPUT_DIR || "dist"
-  )
+  .version(version, '-v, --version')
+  .usage('[options] <file ...>')
+  .option('-c, --chromium <path>', 'The chromium path', resolveArgPath)
+  .option('-o, --output [dir]', 'The output dir path', resolveArgPath)
   .parse(process.argv);
 
-const DIR_PATH = path.join(process.cwd(), program.output);
+function getChromiumPath() {
+  return program.chromium || resolveArgPath(process.env.CHROMIUM_PATH);
+}
+
+function getOuputDirPath() {
+  return program.output || resolveArgPath(process.env.OUTPUT_DIR || 'dist');
+}
+
+const CHROMIUM_PATH = getChromiumPath();
+
+if (!CHROMIUM_PATH) {
+  throw Error('Miss chromium path.');
+}
+
+const DIR_PATH = getOuputDirPath();
 
 try {
   fs.readdirSync(DIR_PATH);
@@ -32,16 +36,16 @@ try {
   fs.mkdirSync(DIR_PATH);
 }
 
-const OUT_PATH = path.join(program.output, "china_address_code.json");
+const OUT_PATH = path.join(program.output, 'china_address_code.json');
 const ALL_PROVINCE_PATH = path.join(
   program.output,
-  "china_province_address_code.json"
+  'china_province_address_code.json',
 );
-const ALL_CITY_PATH = path.join(program.output, "china_city_address_code.json");
-const ALL_AREA_PATH = path.join(program.output, "china_area_address_code.json");
+const ALL_CITY_PATH = path.join(program.output, 'china_city_address_code.json');
+const ALL_AREA_PATH = path.join(program.output, 'china_area_address_code.json');
 const TREE_STRUCT_PATH = path.join(
   program.output,
-  "china_address_code_tree.json"
+  'china_address_code_tree.json',
 );
 
 main({
@@ -50,5 +54,5 @@ main({
   allProvincePath: ALL_PROVINCE_PATH,
   allCityPath: ALL_CITY_PATH,
   allAreaPath: ALL_AREA_PATH,
-  treeStructPath: TREE_STRUCT_PATH
+  treeStructPath: TREE_STRUCT_PATH,
 });
